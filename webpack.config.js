b56c021f1730b3b95387e989b1110ed69b5d881e
IvanRezko <em000406@g.bstu.by>
2024-10-17 12:41:51 +0300

    Feat: add basic files
    Добавлена базовые файлы проекта

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssPlugin = require('mini-css-extract-plugin');
+
+const devServer= isDev =>
+isDev
+    ? {
+        devServer: {
+            open:true,
+            hot: true,
+            port: 8080,
+            static: path.join(__dirname, 'dest'),
+        },
+    }
+    : {};
+
+module.exports=(env, argv) => ({
+    plugins: [
+        new HtmlWebpackPlugin({ template:
+            'src/index.html' }),
+            new CopyPlugin({
+                patterns: [
+                    { from: 'src/assets', to: 'assets'},
+                    { from: 'src/404.html', to: '404.html'},
+                ],
+            }),
+            new MiniCssPlugin({filename: 'style.css'})
+    ],
+    entry: './src/script.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                test: /\.scss$/i,
+                use: 'ts-loader',
+                use: [MiniCssPlugin.loader, {
+                     loader: 'css-loader',
+                     options: {
+                     url: false
+                     }
+                     }, 'sass-loader' ],
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'script.js',
+        path: path.resolve(__dirname, 'dest'),
+        clean: true,
+    },
+    mode: argv.mode ==='development' ? 'development' :
+    'production',
+            devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+            ...devServer(argv.mode ==='development'),
+});
